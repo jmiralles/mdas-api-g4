d6192a7ed080c485318919e2097858afb4a6ec08
Christian Soler <solero93@gmail.com>
2019-05-02 21:27:23 +0200

    Created test of happy path

--- src/test/java/poke/PokemonTypeFinderTest.java
+++ src/test/java/poke/PokemonTypeFinderTest.java
@@ -1,21 +1,33 @@
 package poke;
 
-import org.assertj.core.util.Lists;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PokemonTypeFinderTest {
   private PokemonTypeFinder pokemonTypeFinder;
+  @Mock
+  private PokemonTypeRepository pokemonTypeRepository;
 
-  @Before
-  public void setUp() {
-    //pokemonTypeFinder = new PokemonTypeFinder();
-  }
+  private final int VALID_POKEMON_ID = 1;
+  private final List<String> VALID_TYPE_OUTPUT = Collections.singletonList("output");
 
   @Test
   public void getPokemonType_returnsCorrect_WhenPokemonExists() {
-    //assertThat(pokemonTypeFinder.invoke(1), is(Lists.newArrayList("poison", "")))
+    when(pokemonTypeRepository.find(VALID_POKEMON_ID)).thenReturn(VALID_TYPE_OUTPUT);
+
+    pokemonTypeFinder = new PokemonTypeFinder(pokemonTypeRepository);
+    List<String> actualResult = pokemonTypeFinder.invoke(VALID_POKEMON_ID);
+    verify(pokemonTypeRepository, times(1)).find(VALID_POKEMON_ID);
+    assertThat(actualResult, is(VALID_TYPE_OUTPUT));
   }
 }
